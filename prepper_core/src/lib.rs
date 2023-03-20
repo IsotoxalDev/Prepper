493ddacaf3affa00a0fdbc81161e9aacdf3b9659
Isotoxal <62714538+IsotoxalDev@users.noreply.github.com>
2023-03-21 00:38:31 +0530

    Add a stringy question 🪢

--- prepper_core/src/lib.rs
+++ prepper_core/src/lib.rs
@@ -24,17 +24,24 @@ impl Question {
         let answer = Answer::MCQ(MCQChoices::new(answers)?);
         Ok(Self { prompt, answer })
     }
+    fn new_msq(prompt: String, answers: Vec<String>, correct: Vec<usize>) -> Result<Question> {
+        let answer = Answer::MSQ(MSQChoices::new(answers, correct)?);
+        Ok(Self { prompt, answer })
+    }
+    fn new_integer(prompt: String, answer: isize) -> Result<Question> {
+        let answer = Answer::Integer(answer);
+        Ok(Self { prompt, answer })
+    }
+    fn new_formula(prompt: String, answer: String) -> Result<Question> {
+        let answer = Answer::Formula(answer);
+        Ok(Self { prompt, answer })
+    }
+    fn new_string(prompt: String, answer: String) -> Result<Question> {
+        let answer = Answer::String(answer);
+        Ok(Self { prompt, answer })
+    }
 }
 
 pub fn get_question() -> Question {
-    Question::new_mcq(
-        "I am stupid ?".into(),
-        vec![
-            "Yes".into(),
-            "No".into(),
-            "Probably".into(),
-            "Probably not".into(),
-        ],
-    )
-    .unwrap()
+    Question::new_string("How is apple spelled?".to_string(), "Apple".to_string()).unwrap()
 }
